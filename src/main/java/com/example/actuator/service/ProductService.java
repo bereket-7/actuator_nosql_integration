ede378aaa178d1ecd9de3af3ab27138c69faf19d
bereket-7 <bekhonelign@gmail.com>
2024-11-09 04:52:50 +0300

    product service

--- src/main/java/com/example/actuator/service/ProductService.java
+++ src/main/java/com/example/actuator/service/ProductService.java
@@ -8,7 +8,11 @@ import java.util.List;
 
 @Service
 public class ProductService {
-    private ProductRepository productRepository;
+    private final ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
 
     public List<Product> getAllProducts() {
         return productRepository.findAll();
