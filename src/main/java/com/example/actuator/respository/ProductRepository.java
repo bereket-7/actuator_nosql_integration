b484b9388bba928fe8fbf385a2f9ef5a2f47e0f8
bereket-7 <bekhonelign@gmail.com>
2024-11-09 03:18:11 +0300

    Product repo

---
+++ src/main/java/com/example/actuator/respository/ProductRepository.java
@@ -0,0 +1,12 @@
+package com.example.actuator.respository;
+
+import com.example.actuator.model.Product;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ProductRepository extends MongoRepository<Product, String> {
+    List<Product> findByNameContaining(String name);
+}
