dd8a66357b49e7185c1f93dd16c3e75c774ab5ba
bereket-7 <bekhonelign@gmail.com>
2024-11-09 03:18:04 +0300

    Product controller

---
+++ src/main/java/com/example/actuator/controller/ProductController.java
@@ -0,0 +1,38 @@
+package com.example.actuator.controller;
+
+import com.example.actuator.model.Product;
+import com.example.actuator.service.ProductService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping
+    public List<Product> getAllProducts() {
+        return productService.getAllProducts();
+    }
+
+    @GetMapping("/{id}")
+    public Product getProductById(@PathVariable String id) {
+        return productService.getProductById(id);
+    }
+
+    @PostMapping
+    public Product addProduct(@RequestBody Product product) {
+        return productService.addProduct(product);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteProduct(@PathVariable String id) {
+        productService.deleteProduct(id);
+    }
+}
