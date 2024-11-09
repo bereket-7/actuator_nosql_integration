d34b1762a11216883b5e42d549f6980d425bf135
bereket-7 <bekhonelign@gmail.com>
2024-11-09 03:18:24 +0300

    Product service

---
+++ src/main/java/com/example/actuator/service/ProductService.java
@@ -0,0 +1,28 @@
+package com.example.actuator.service;
+
+import com.example.actuator.model.Product;
+import com.example.actuator.respository.ProductRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+    private ProductRepository productRepository;
+
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    public Product getProductById(String id) {
+        return productRepository.findById(id).orElse(null);
+    }
+
+    public Product addProduct(Product product) {
+        return productRepository.save(product);
+    }
+
+    public void deleteProduct(String id) {
+        productRepository.deleteById(id);
+    }
+}
